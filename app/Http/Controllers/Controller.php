78e5508fe0c9bedba3fe5563ae4b1ab816e70953
chan\admin <s707071127@gmail.com>
2017-10-19 16:00:24 +0800

    從 Controller 將參數傳遞至 view

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index($name = 'Everybody')
+    {
+        $data=['name'=> $name];
+        return view('hello.index',$data);
+    }
 }
+
